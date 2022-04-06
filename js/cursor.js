8f0796a66395b9d23dbd748e2b31072ed6bc8072
Yonatan Eshed <yonatan.eshed@gmail.com>
2022-04-06 21:03:37 +0300

    change the site design

--- js/cursor.js
+++
@@ -1,6 +0,0 @@
-const cursor = document.querySelector('.cursor');
-const cursorSize = 10
-
-document.addEventListener('mousemove', e =>{
-    cursor.setAttribute("style", `top: ${e.pageY - cursorSize/2}px; left: ${e.pageX - cursorSize/2}px;`);
-});
--- js/script.js
+++
@@ -1,4 +0,0 @@
-
-window.addEventListener("DOMContentLoaded", () => {
-  document.body.style.setProperty("--transition", "0.3s ease 0s");
-});
